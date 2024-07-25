16ba1c80d5075c1287f517cbd0edd9bd3104fd2c
Saurabh Nair <107759922+saurabh13113@users.noreply.github.com>
2024-07-25 09:20:53 -0400

    adding resume download

--- app/page.jsx
+++ app/page.jsx
@@ -19,10 +19,12 @@ const Home = () => {
            </h1>
            <p className="max-w-[500px] mb-9 text-white/80"> I excel at everything, Hire me please</p>
         <div className="flex flex-col xl:flex-row items-center gap-8">
-          <Button variant="outline" size="lg" className="uppercase flex items-center gap-2" path="/assets/resume.pdf">
-            <span> Here is my Resume! </span>
-            <FiDownload className = "text-xl" path="/assets/resume.pdf" />
-          </Button>
+          <a href="/assets/resume.pdf" download="resume.pdf">
+            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+              <span> Here is my Resume! </span>
+              <FiDownload className="text-xl" />
+            </Button>
+          </a>
           <div className="mb-8 xl:mb-0">
             <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full
             flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary 
