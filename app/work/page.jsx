d9fb131a0fa1104841287f0a7d525f11d858140a
Saurabh Nair <107759922+saurabh13113@users.noreply.github.com>
2024-08-12 08:57:49 +0400

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -24,44 +24,48 @@ const projects = [
     {num: '03', category: 'Backend', title:'Web-Scraper for Premier League Data',description:'Summer Project that uses BeautifulSoup and pandas to obtain premier league data for the past few seasons using web-scraping.',
         stack: [{name:"Python"},{name:"Beautiful Soup"},{name:"Jupyter"},{name:"Pandas"}], image: "/assets/work/Pic13.png",
         live:"https://github.com/saurabh13113/webscraper-premier-league", github: "https://github.com/saurabh13113/webscraper-premier-league"},
+
+    {num: '04', category: 'FullStack', title:'Math Chat Bot using Llama AI',description:'Summer Project that uses Meta Llama to build an AI chatbot to help students out with math problems and to practice math using next.js, react, vercel.',
+        stack: [{name:"React"},{name:"Next.js"},{name:"Material UI"},{name:"Llama AI"},{name:"AWS EC2"}], image: "/assets/work/Pic14.png",
+        live:"https://math-chatbot-eta.vercel.app/", github: "https://github.com/saurabh13113/math-chatbot/tree/main"},
     
-    {num: '04', category: 'FullStack', title:'UAE Metro Ticketer',description:'Highschool Project that allows customers to purchase UAE metro tickets',
+    {num: '05', category: 'FullStack', title:'UAE Metro Ticketer',description:'Highschool Project that allows customers to purchase UAE metro tickets',
         stack: [{name:"Python"},{name:"TKinter"}], image: "/assets/work/Pic10.png",
         live:"https://github.com/saurabh13113/uaemetro-ticketer", github: "https://github.com/saurabh13113/uaemetro-ticketer"},
 
-    {num: '05', category: 'FullStack', title:'Pantry Tracker',description:'Summer Project that is a pantry management application built with Next.js, Material UI, and Firebase',
+    {num: '06', category: 'FullStack', title:'Pantry Tracker',description:'Summer Project that is a pantry management application built with Next.js, Material UI, and Firebase',
         stack: [{name:"React"},{name:"Next.js"},{name:"Material UI"},{name:"Firebase"}], image: "/assets/work/Pic11.png",
         live:"https://pantry-app-lac.vercel.app/", github: "https://github.com/saurabh13113/pantry-app"},
 
-    {num: '06', category: 'Backend', title:'Huffman Tree File Compressor',description:'University project that decompresses file sizes using Huffman trees.',
+    {num: '07', category: 'Backend', title:'Huffman Tree File Compressor',description:'University project that decompresses file sizes using Huffman trees.',
         stack: [{name:"Python"},], image: "/assets/work/Pic2.png",
         live:"https://github.com/saurabh13113/huffman-compressor-tree-", github: "https://github.com/saurabh13113/huffman-compressor-tree-"},
 
-    {num: '07', category: 'Frontend', title:'Mobile System Tracker',description:'University assignment to track a mobile company and its customers including a visualizer to do the same',
+    {num: '08', category: 'Frontend', title:'Mobile System Tracker',description:'University assignment to track a mobile company and its customers including a visualizer to do the same',
         stack: [{name:"Python"},{name:"PyGame"}], image: "/assets/work/pic1.png",
         live:"https://github.com/saurabh13113/mobile-companytracker", github: "https://github.com/saurabh13113/mobile-companytracker"},
 
-    {num: '08', category: 'FullStack', title:'TreeMap File Organizer',description:'University project that makes use of file system trees and treemaps to organize files and folders using a visualizer.',
+    {num: '09', category: 'FullStack', title:'TreeMap File Organizer',description:'University project that makes use of file system trees and treemaps to organize files and folders using a visualizer.',
         stack: [{name:"Python"},{name:"PyGame"}], image: "/assets/work/Pic4.png",
         live:"https://github.com/saurabh13113/treemap-file-organizer-tree-", github: "https://github.com/saurabh13113/treemap-file-organizer-tree-"},
 
-    {num: '09', category: 'Frontend', title:'Driver Rider Pairer',description:'University project that matches up drivers and riders based on locational information',
+    {num: '10', category: 'Frontend', title:'Driver Rider Pairer',description:'University project that matches up drivers and riders based on locational information',
         stack: [{name:"Python"}], image: "/assets/work/Pic8.png",
         live:"https://github.com/saurabh13113/uber-driver-rider-pairer", github: "https://github.com/saurabh13113/uber-driver-rider-pairer"},
 
-    {num: '10', category: 'FullStack', title:'MindSnatcher Player Game',description:'University assignment to build a game over three months using Java and JavaFX following the principle of Agile Software development in a team of 4.',
+    {num: '11', category: 'FullStack', title:'MindSnatcher Player Game',description:'University assignment to build a game over three months using Java and JavaFX following the principle of Agile Software development in a team of 4.',
         stack: [{name:"Java"},{name:"JavaFx"},{name:"PlayHT"}], image: "/assets/work/Pic3.png",
         live:"https://github.com/saurabh13113/mindsnatcher-game", github: "https://github.com/saurabh13113/mindsnatcher-game"},
 
-    {num: '11', category: 'Backend', title:'Add Echo Program',description:'University project that removes vocals and adds an echoing sound to a audio(wav) file by decoding bit information.',
+    {num: '12', category: 'Backend', title:'Add Echo Program',description:'University project that removes vocals and adds an echoing sound to a audio(wav) file by decoding bit information.',
         stack: [{name:"C"}], image: "/assets/work/Pic7.png",
         live:"https://github.com/saurabh13113/add-echo", github: "https://github.com/saurabh13113/add-echo"},
 
-    {num: '12', category: 'Backend', title:'tsh Mini Shell',description:'University project making a replica mini shell command prompt using C',
+    {num: '13', category: 'Backend', title:'tsh Mini Shell',description:'University project making a replica mini shell command prompt using C',
         stack: [{name:"C"}], image: "/assets/work/Pic6.png",
         live:"https://github.com/saurabh13113/tsh-mini-shell", github: "https://github.com/saurabh13113/tsh-mini-shell"},
 
-    {num: '13', category: 'Backend', title:'Multiplayer Server Game',description:'University project that creates a local server on the Uoft servers and allows users to join and play a simple battle game',
+    {num: '14', category: 'Backend', title:'Multiplayer Server Game',description:'University project that creates a local server on the Uoft servers and allows users to join and play a simple battle game',
         stack: [{name:"C"}], image: "/assets/work/Pic5.png",
         live:"https://github.com/saurabh13113/multiplayer-server-game", github: "https://github.com/saurabh13113/multiplayer-server-game"},
 ];
