ea490cd01046874568625ab00ba5338a53db6405
Saurabh Nair <107759922+saurabh13113@users.noreply.github.com>
2024-12-29 18:25:13 -0500

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -45,31 +45,47 @@ const projects = [
         stack: [{name:"Python"},], image: "/assets/work/Pic2.png",
         live:"https://github.com/saurabh13113/huffman-compressor-tree-", github: "https://github.com/saurabh13113/huffman-compressor-tree-"},
 
-    {num: '09', category: 'Frontend', title:'Mobile System Tracker',description:'University assignment to track a mobile company and its customers including a visualizer to do the same',
+    {num: '09', category: 'Backend', title:'Hua Rong Dao Puzzle Solver',description:'University project that solves Hua-Rong-Dao Puzzle using State Space Searching.',
+        stack: [{name:"Python"},], image: "/assets/work/Pic16.png",
+        live:"https://github.com/saurabh13113/Hua-Rong-Dao-Solver", github: "https://github.com/saurabh13113/Hua-Rong-Dao-Solver"},
+
+    {num: '10', category: 'Backend', title:'Checkers Game Solver',description:'University project that solves Checkers EndGame using Game Tree Searching.',
+        stack: [{name:"Python"},], image: "/assets/work/Pic17.png",
+        live:"https://github.com/saurabh13113/checkers-solver", github: "https://github.com/saurabh13113/checkers-solver"},
+
+    {num: '11', category: 'Backend', title:'Battle-Ship Solitaire Solver',description:'University project that solves Battle-Ship Solitaire Endgame using Constraint Satisfaction and GAC.',
+        stack: [{name:"Python"},], image: "/assets/work/Pic18.png",
+        live:"https://github.com/saurabh13113/battleship-solitaire-solver", github: "https://github.com/saurabh13113/battleship-solitaire-solver"},
+    
+    {num: '12', category: 'Backend', title:'Naive Bayes Salary Predictor',description:'University project that predicts salaries by building a Naive Bayesian Model',
+        stack: [{name:"Python"},], image: "/assets/work/Pic19.png",
+        live:"https://github.com/saurabh13113/Naive-Bayes_Model-", github: "https://github.com/saurabh13113/Naive-Bayes_Model"},
+
+    {num: '13', category: 'Frontend', title:'Mobile System Tracker',description:'University assignment to track a mobile company and its customers including a visualizer to do the same',
         stack: [{name:"Python"},{name:"PyGame"}], image: "/assets/work/pic1.png",
         live:"https://github.com/saurabh13113/mobile-companytracker", github: "https://github.com/saurabh13113/mobile-companytracker"},
 
-    {num: '10', category: 'FullStack', title:'TreeMap File Organizer',description:'University project that makes use of file system trees and treemaps to organize files and folders using a visualizer.',
+    {num: '14', category: 'FullStack', title:'TreeMap File Organizer',description:'University project that makes use of file system trees and treemaps to organize files and folders using a visualizer.',
         stack: [{name:"Python"},{name:"PyGame"}], image: "/assets/work/Pic4.png",
         live:"https://github.com/saurabh13113/treemap-file-organizer-tree-", github: "https://github.com/saurabh13113/treemap-file-organizer-tree-"},
 
-    {num: '11', category: 'Frontend', title:'Driver Rider Pairer',description:'University project that matches up drivers and riders based on locational information',
+    {num: '15', category: 'Frontend', title:'Driver Rider Pairer',description:'University project that matches up drivers and riders based on locational information',
         stack: [{name:"Python"}], image: "/assets/work/Pic8.png",
         live:"https://github.com/saurabh13113/uber-driver-rider-pairer", github: "https://github.com/saurabh13113/uber-driver-rider-pairer"},
 
-    {num: '12', category: 'FullStack', title:'MindSnatcher Player Game',description:'University assignment to build a game over three months using Java and JavaFX following the principle of Agile Software development in a team of 4.',
+    {num: '16', category: 'FullStack', title:'MindSnatcher Player Game',description:'University assignment to build a game over three months using Java and JavaFX following the principle of Agile Software development in a team of 4.',
         stack: [{name:"Java"},{name:"JavaFx"},{name:"PlayHT"}], image: "/assets/work/Pic3.png",
         live:"https://github.com/saurabh13113/mindsnatcher-game", github: "https://github.com/saurabh13113/mindsnatcher-game"},
 
-    {num: '13', category: 'Backend', title:'Add Echo Program',description:'University project that removes vocals and adds an echoing sound to a audio(wav) file by decoding bit information.',
+    {num: '17', category: 'Backend', title:'Add Echo Program',description:'University project that removes vocals and adds an echoing sound to a audio(wav) file by decoding bit information.',
         stack: [{name:"C"}], image: "/assets/work/Pic7.png",
         live:"https://github.com/saurabh13113/add-echo", github: "https://github.com/saurabh13113/add-echo"},
 
-    {num: '14', category: 'Backend', title:'tsh Mini Shell',description:'University project making a replica mini shell command prompt using C',
+    {num: '18', category: 'Backend', title:'tsh Mini Shell',description:'University project making a replica mini shell command prompt using C',
         stack: [{name:"C"}], image: "/assets/work/Pic6.png",
         live:"https://github.com/saurabh13113/tsh-mini-shell", github: "https://github.com/saurabh13113/tsh-mini-shell"},
 
-    {num: '15', category: 'Backend', title:'Multiplayer Server Game',description:'University project that creates a local server on the Uoft servers and allows users to join and play a simple battle game',
+    {num: '19', category: 'Backend', title:'Multiplayer Server Game',description:'University project that creates a local server on the Uoft servers and allows users to join and play a simple battle game',
         stack: [{name:"C"}], image: "/assets/work/Pic5.png",
         live:"https://github.com/saurabh13113/multiplayer-server-game", github: "https://github.com/saurabh13113/multiplayer-server-game"},
 ];
